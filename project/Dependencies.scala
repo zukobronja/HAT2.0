95b648acf6ed101504d51225e06ca986ee66efc7
andrius <andrius.aucinas@gmail.com>
2018-02-18 20:50:58 +0000

    updates dependencies and patches up tests

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -48,8 +48,8 @@ object Dependencies {
       val test = "com.typesafe.play" %% "play-test" % version
       val specs2 = "com.typesafe.play" %% "play-specs2" % version
       val jdbc = "com.typesafe.play" %% "play-jdbc" % version
-      val json = "com.typesafe.play" %% "play-json" % version
-      val jsonJoda = "com.typesafe.play" %% "play-json-joda" % version
+      val json = "com.typesafe.play" %% "play-json" % "2.6.8"
+      val jsonJoda = "com.typesafe.play" %% "play-json-joda" % "2.6.8"
       val mailer = "com.typesafe.play" %% "play-mailer" % "6.0.1"
       val mailerGuice = "com.typesafe.play" %% "play-mailer-guice" % "6.0.1"
 
@@ -57,7 +57,7 @@ object Dependencies {
       val playGuard = "com.digitaltangible" %% "play-guard" % "2.1.0"
 
       object Jwt {
-        private val bouncyCastleVersion = "1.58"
+        private val bouncyCastleVersion = "1.59"
         val bouncyCastle = "org.bouncycastle" % "bcprov-jdk15on" % bouncyCastleVersion
         val bouncyCastlePkix = "org.bouncycastle" % "bcpkix-jdk15on" % bouncyCastleVersion
         val atlassianJwtVersion = "1.6.2"
@@ -76,7 +76,7 @@ object Dependencies {
     }
 
     object Specs2 {
-      private val version = "3.8.9"
+      private val version = "3.9.5"
       val core = "org.specs2" %% "specs2-core" % version
       val matcherExtra = "org.specs2" %% "specs2-matcher-extra" % version
       val mock = "org.specs2" %% "specs2-mock" % version
@@ -85,9 +85,9 @@ object Dependencies {
     object Utils {
       val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
       val awsJavaSdk = "com.amazonaws" % "aws-java-sdk" % "1.10.64"
-      val awsJavaS3Sdk = "com.amazonaws" % "aws-java-sdk-s3" % "1.11.123"
-      val prettyTime = "org.ocpsoft.prettytime" % "prettytime" % "3.2.7.Final"
-      val nbvcxz = "me.gosimple" % "nbvcxz" % "1.3.4"
+      val awsJavaS3Sdk = "com.amazonaws" % "aws-java-sdk-s3" % "1.11.280"
+      val prettyTime = "org.ocpsoft.prettytime" % "prettytime" % "4.0.1.Final"
+      val nbvcxz = "me.gosimple" % "nbvcxz" % "1.4.0"
     }
 
     object Akka {
@@ -106,6 +106,6 @@ object Dependencies {
       val codegen = "org.hatdex" %% "slick-postgres-driver" % "0.0.7-SNAPSHOT"
     }
 
-    val scalaGuice = "net.codingwell" %% "scala-guice" % "4.1.0"
+    val scalaGuice = "net.codingwell" %% "scala-guice" % "4.1.1"
   }
 }
